6460efbc29ef2abc74307b0ca26cf9cc8f7c41b3
Dominik Stasiak <d.stasiak436@gmail.com>
2024-02-24 14:22:44 +0100

    add: command an py file for adding sets

--- Terminal/commands.cpp
+++ Terminal/commands.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <filesystem>
 #include "commands.hpp"
 
 
@@ -19,6 +20,9 @@ void execute(const char *command) {
     } else if (strncmp(command, "start ", 5) == 0) {
         const char *setName = command + 6;
         start(setName);
+    } else if (strncmp(command, "add ", 3) == 0) {
+        const char *setName = command + 4;
+        add(setName);
     } else {
         std::cout << "Unknown command: " << command << std::endl;
     }
@@ -33,20 +37,35 @@ void exitS() {
     std::exit(0);
 }
 
-void start(const std::string& setName) {
-    if (!setName.empty()) {
-        std::cout << "Starting set: " << setName << std::endl;
-        // to do
-    } else {
-        std::cerr << "Usage: start [set]" << std::endl;
-    }
-}
-
 void help() {
     std::cout << "--------------------------------------" << std::endl;
     std::cout << "All commands:" << std::endl;
     std::cout << "help - show all commands" << std::endl;
     std::cout << "start [set] - run one of existing sets" << std::endl;
+    std::cout << "add [set] - add new set" << std::endl;
     std::cout << "exit - turn off Neutrino" << std::endl;
     std::cout << "--------------------------------------" << std::endl;
 }
+
+void start(const std::string& setName) {
+    if (!setName.empty()) {
+        std::cout << "Starting set: " << setName << std::endl;
+        //to do
+    } else {
+        std::cerr << "Usage: start [set]" << std::endl;
+    }
+}
+
+void add(const std::string& setName) {
+    //to do: check if set is already existing
+    if (!setName.empty()) {
+        std::string scriptPath = "../sets.py"; 
+        std::string command = "python " + std::filesystem::absolute(scriptPath).string() + " " + setName;
+        int result = system(command.c_str());
+
+        if (result == 1) {
+            std::cerr << "Error" << std::endl;
+        }
+    } else {
+        std::cerr << "Usage: add [set]" << std::endl;
+    }}
--- Terminal/commands.hpp
+++ Terminal/commands.hpp
@@ -1,10 +1,13 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+#include <string>
+
 void execute(const char *command);
 void echo(const char *text);
 void exitS();
 void help();
 void start(const std::string& setName);
+void add(const std::string& setName);
 
 #endif
