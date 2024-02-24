90a00789f63ded7f6be3639081c458e3846f0575
Dominik Stasiak <d.stasiak436@gmail.com>
2024-02-24 13:54:19 +0100

    add: start command

--- Terminal/commands.cpp
+++ Terminal/commands.cpp
@@ -16,6 +16,9 @@ void execute(const char *command) {
         exitS();
     } else if (strcmp(command, "help") == 0) {
         help();
+    } else if (strncmp(command, "start ", 5) == 0) {
+        const char *setName = command + 6;
+        start(setName);
     } else {
         std::cout << "Unknown command: " << command << std::endl;
     }
@@ -30,11 +33,20 @@ void exitS() {
     std::exit(0);
 }
 
+void start(const std::string& setName) {
+    if (!setName.empty()) {
+        std::cout << "Starting set: " << setName << std::endl;
+        // to do
+    } else {
+        std::cerr << "Usage: start [set]" << std::endl;
+    }
+}
+
 void help() {
     std::cout << "--------------------------------------" << std::endl;
     std::cout << "All commands:" << std::endl;
     std::cout << "help - show all commands" << std::endl;
-    std::cout << "echo [text] - write a text" << std::endl;
+    std::cout << "start [set] - run one of existing sets" << std::endl;
     std::cout << "exit - turn off Neutrino" << std::endl;
     std::cout << "--------------------------------------" << std::endl;
 }
--- Terminal/commands.hpp
+++ Terminal/commands.hpp
@@ -5,5 +5,6 @@ void execute(const char *command);
 void echo(const char *text);
 void exitS();
 void help();
+void start(const std::string& setName);
 
 #endif
